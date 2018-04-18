56732d0d1579c92479b45588d891dda1298c4a10
Wei <weinini007@gmail.com>
2018-04-18 23:50:48 +0200

    Add image move click js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 const slideImage = document.querySelector(".image");
 
-slideImage.addEventListener("click", () => slideImage.classList.)
+slideImage.addEventListener("click", () => slideImage.classList.add("image-slide"));
