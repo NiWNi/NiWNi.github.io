abd79c9a774a9f949b12f8a48016ea69c1877a3f
Wei <weinini007@gmail.com>
2018-04-19 12:45:33 +0200

    Add click function on image

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 const slideImage = document.querySelector(".image");
 
-slideImage.addEventListener("click", () => slideImage.classList.add("image-slide"));
+function checkClass() {
+    if (slideImage.classList.contains("image-slide")) {
+        return slideImage.classList.remove("image-slide");
+    } else {
+        return slideImage.classList.add("image-slide");
+    }
+}
+
+slideImage.addEventListener("click", checkClass);
