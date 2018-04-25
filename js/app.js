5e7672f8805451a647f65898263ca48918e1442d
Wei <weinini007@gmail.com>
2018-04-25 19:59:15 +0200

    Add smooth scrolling jquery

--- js/app.js
+++ js/app.js
@@ -9,3 +9,34 @@ function checkClass() {
 }
 
 slideImage.addEventListener("click", checkClass);
+
+$(document).ready(function(){
+    $("#hamburgerIcon").click(function(){ //turns hamburger icon to X and back
+      $("#line1").toggleClass("active");
+      $("#line2").toggleClass("active");
+      $("#line3").toggleClass("active");
+    });
+  
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
